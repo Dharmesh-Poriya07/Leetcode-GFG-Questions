2cb5e2bf611676d2d3ecc76bf110f83aefc0024c
Dharmesh Poriya <84856389+Dharmesh-Poriya07@users.noreply.github.com>
2022-07-25 18:56:34 +0530

    Time: 7 ms (90.42%), Space: 13.5 MB (98.09%) - LeetHub

---
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -0,0 +1,63 @@
+class Solution {
+private:
+    int firstOccurance(vector<int>& nums, int target){
+        int index = -1;
+        int low,high;
+        low = 0,high = nums.size()-1;
+        while(low<=high){
+            int mid = low + (high-low)/2;
+            if(nums[mid]==target){
+                if(0==mid){
+                    index = 0;
+                    break;
+                }else{
+                    if(nums[mid-1]==target){
+                        index = mid-1;
+                        high = mid-2; 
+                    }else{
+                        index = mid;
+                        break;
+                    }
+                }
+            }else if(nums[mid]>target){
+                high = mid-1;
+            }else{
+                low = mid+1;
+            }
+        }
+        return index;
+    }
+    int lastOccurance(vector<int>& nums, int target){
+        int index = -1;
+        int low,high;
+        low = 0,high = nums.size()-1;
+        while(low<=high){
+            int mid = low + (high-low)/2;
+            if(nums[mid]==target){
+                if(nums.size()-1==mid){
+                    index = nums.size()-1;
+                    break;
+                }else{
+                    if(nums[mid+1]==target){
+                        index = mid+1;
+                        low = mid+2; 
+                    }else{
+                        index = mid;
+                        break;
+                    }
+                }
+            }else if(nums[mid]>target){
+                high = mid-1;
+            }else{
+                low = mid+1;
+            }
+        }
+        return index;
+    }
+public:
+    vector<int> searchRange(vector<int>& nums, int target) {
+        int firsto = firstOccurance(nums,target);
+        int lasto = lastOccurance(nums,target);
+        return {firsto,lasto};
+    }
+};
