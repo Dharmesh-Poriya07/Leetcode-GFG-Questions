1cfb47ba387279375c723c807dd1d4ba870e698f
Dharmesh Poriya <84856389+Dharmesh-Poriya07@users.noreply.github.com>
2022-08-01 17:17:02 +0530

    Time: 3 ms (44.81%), Space: 6.5 MB (26.54%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -2,15 +2,9 @@ class Solution {
 private:
     vector<vector<int>> dp;
     int helper(int row,int col,int m,int n){
-        if(row==m || col==n){
-            return 0;
-        }
-        if(-1!=dp[row][col]){
-            return dp[row][col];
-        }
-        if(row==m-1 && col==n-1){
-            return 1;
-        }
+        if(row==m || col==n) return 0;
+        if(-1!=dp[row][col]) return dp[row][col];
+        if(row==m-1 && col==n-1) return 1;
         int right = helper(row,col+1,m,n);
         int bottom = helper(row+1,col,m,n);
         return dp[row][col] = right+bottom;
