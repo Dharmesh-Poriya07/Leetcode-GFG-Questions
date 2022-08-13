cb94a21567fee909eb6af73f13acd756b0362118
Dharmesh Poriya <84856389+Dharmesh-Poriya07@users.noreply.github.com>
2022-08-13 15:33:38 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (95.04%) - LeetHub

--- 69-sqrtx/69-sqrtx.cpp
+++ 69-sqrtx/69-sqrtx.cpp
@@ -1,6 +1,21 @@
 class Solution {
 public:
     int mySqrt(int x) {
-        return floor(sqrt(x));
+        int ans=0;
+        int low = 1,high=x;
+        while(low<=high){
+            long long mid = low + (high-low)/2;
+            long long int sq = mid*mid;
+            if(sq==x){
+                ans = mid;
+                break;
+            }else if(sq>x){
+                high = mid-1;
+            }else{
+                ans = mid;
+                low = mid+1;
+            }
+        }
+        return ans;
     }
 };
