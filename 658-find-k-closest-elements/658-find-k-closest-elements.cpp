d057346e796716880b0ecac398c4507b46fb9b8d
Dharmesh Poriya <84856389+Dharmesh-Poriya07@users.noreply.github.com>
2022-08-15 13:11:04 +0530

    Time: 51 ms (88.79%), Space: 31.9 MB (60.10%) - LeetHub

---
+++ 658-find-k-closest-elements/658-find-k-closest-elements.cpp
@@ -0,0 +1,57 @@
+class Solution {
+private:
+    vector<int> bSearch(vector<int>& arr, int x){
+        int low = 0;
+        int high = arr.size()-1;
+        while(low<=high){
+            int mid = low + (high-low)/2;
+            if(arr[mid]==x){
+                return {mid-1,mid};
+            }else if(x>arr[mid]){
+                low = mid+1;
+            }else{
+                high = mid-1;
+            }
+        }
+        return {high,low};
+    }
+public:
+    vector<int> findClosestElements(vector<int>& arr, int k, int x) {
+        vector<int> ans;
+        if(x<=arr[0]){
+            ans = vector<int>(arr.begin(),arr.begin()+k);
+            return ans;
+        }
+        if(x>=arr.back()){
+            ans = vector<int>(arr.end()-k,arr.end());
+            return ans;
+        }
+        auto lh = bSearch(arr,x);
+        int high = lh[1];
+        int low = lh[0];
+        while(0<k){
+            if(high==arr.size()){
+                while(0<k){
+                    ans.push_back(arr[low--]);
+                    k--;
+                }
+                break;
+            }
+            if(low==-1){
+                while(0<k){
+                    ans.push_back(arr[high++]);
+                    k--;
+                }
+                break;
+            }
+            if(x-arr[low]<=arr[high]-x){
+                ans.push_back(arr[low--]);
+            }else{
+                ans.push_back(arr[high++]);
+            }
+            k--;
+        }
+        sort(ans.begin(),ans.end());
+        return ans;
+    }
+};
