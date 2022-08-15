6c87a5efc414a418fd5e9e42fd449323f2c9da89
Dharmesh Poriya <84856389+Dharmesh-Poriya07@users.noreply.github.com>
2022-08-15 16:21:22 +0530

    Time: 166 ms (21.92%), Space: 34.1 MB (36.28%) - LeetHub

--- 658-find-k-closest-elements/658-find-k-closest-elements.cpp
+++ 658-find-k-closest-elements/658-find-k-closest-elements.cpp
@@ -1,50 +1,22 @@
 class Solution {
-private:
-    vector<int> bSearch(vector<int>& arr, int x){
-        int low = 0;
-        int high = arr.size()-1;
-        while(low<=high){
-            int mid = low + (high-low)/2;
-            if(arr[mid]==x){
-                return {mid-1,mid};
-            }else if(x>arr[mid]){
-                low = mid+1;
-            }else{
-                high = mid-1;
-            }
-        }
-        return {high,low};
-    }
 public:
     vector<int> findClosestElements(vector<int>& arr, int k, int x) {
         vector<int> ans;
-        if(x<=arr[0]){
-            ans = vector<int>(arr.begin(),arr.begin()+k);
-            return ans;
+        priority_queue<pair<int,int>> pq;
+        for(int i=0;i<k;i++){
+            pq.push({abs(x-arr[i]),arr[i]});
         }
-        if(x>=arr.back()){
-            ans = vector<int>(arr.end()-k,arr.end());
-            return ans;
+        
+        for(int i=k;i<arr.size();i++){
+            int diff = abs(x-arr[i]);
+            if(pq.top().first>diff || (pq.top().first==diff && pq.top().second>arr[i])){
+                pq.pop();
+                pq.push({diff,arr[i]});
+            }
         }
-        auto lh = bSearch(arr,x);
-        int high = lh[1];
-        int low = lh[0];
-        while(0<k){
-            if(high==arr.size()){
-                while(k--)
-                    ans.push_back(arr[low--]);
-                break;
-            }
-            if(low==-1){
-                while(k--)
-                    ans.push_back(arr[high++]);
-                break;
-            }
-            if(x-arr[low]<=arr[high]-x)
-                ans.push_back(arr[low--]);
-            else
-                ans.push_back(arr[high++]);
-            k--;
+        while(!pq.empty()){
+            ans.push_back(pq.top().second);
+            pq.pop();
         }
         sort(ans.begin(),ans.end());
         return ans;
