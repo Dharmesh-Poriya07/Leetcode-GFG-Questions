15e101e6f5b288dc6159a964cda5d4f6ff05118c
Dharmesh Poriya <84856389+Dharmesh-Poriya07@users.noreply.github.com>
2022-08-15 19:32:27 +0530

    Time: 99 ms (60.44%), Space: 25.3 MB (56.51%) - LeetHub

--- 475-heaters/475-heaters.cpp
+++ 475-heaters/475-heaters.cpp
@@ -15,21 +15,6 @@ private:
         }
         return fl;
     }
-    int Ceil(vector<int>& heaters,int x){
-        int low = 0;
-        int high = heaters.size()-1;
-        int cl = -1;
-        while(low<=high){
-            int mid = low + (high-low)/2;
-            if(x<=heaters[mid]){
-                cl = heaters[mid];
-                high = mid-1;
-            }else{
-                low = mid+1;
-            }
-        }
-        return cl;
-    }
 public:
     int findRadius(vector<int>& houses, vector<int>& heaters) {
         sort(heaters.begin(),heaters.end());
@@ -37,14 +22,14 @@ public:
         int radiuse = INT_MIN;
         for(int h=0;houses.size()>h;h++){
             int _floor = Floor(heaters,houses[h]);
-            int _ceil = Ceil(heaters,houses[h]);
+            auto _ceil = lower_bound(heaters.begin(),heaters.end(),houses[h]);
             int nearest;
             if(-1==_floor)
-                nearest = abs(houses[h]-_ceil);
-            else if(-1==_ceil)
+                nearest = abs(houses[h]-*_ceil);
+            else if(heaters.end()==_ceil)
                 nearest = abs(houses[h]-_floor);
             else
-                nearest = min(abs(houses[h]-_floor),abs(houses[h]-_ceil));
+                nearest = min(abs(houses[h]-_floor),abs(houses[h]-*_ceil));
             radiuse = max(radiuse,nearest);
         }
         return radiuse;
