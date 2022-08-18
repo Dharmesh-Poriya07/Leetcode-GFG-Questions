4355801381498344e1a42eba0ddbd023ed56fdb6
Dharmesh Poriya <84856389+Dharmesh-Poriya07@users.noreply.github.com>
2022-08-18 15:33:22 +0530

    Time: 186 ms (23.09%), Space: 47.1 MB (10.57%) - LeetHub

--- 560-subarray-sum-equals-k/560-subarray-sum-equals-k.cpp
+++ 560-subarray-sum-equals-k/560-subarray-sum-equals-k.cpp
@@ -4,8 +4,8 @@ public:
         unordered_map<int,int> presum_freq;
         presum_freq[0]++;
         int count = 0,presum = 0;
-        for(int i=0;i<nums.size();i++){
-            presum += nums[i];
+        for(int num : nums){
+            presum += num;
             if(0<presum_freq[presum-k])
                 count += presum_freq[presum-k];
             presum_freq[presum]++;
