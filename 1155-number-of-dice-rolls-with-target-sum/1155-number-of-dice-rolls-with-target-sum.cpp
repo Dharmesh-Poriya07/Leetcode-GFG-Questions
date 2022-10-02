bc584782d580d90f119906bcc3753e609f88dce4
Dharmesh Poriya <84856389+Dharmesh-Poriya07@users.noreply.github.com>
2022-10-02 10:35:44 +0530

    Time: 63 ms (38.12%), Space: 8.5 MB (45.97%) - LeetHub

--- 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.cpp
+++ 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.cpp
@@ -2,7 +2,7 @@ class Solution {
 private:
     vector<vector<int>> dp;
     int m = 1e9+7;
-    long int modAdd(long a,long b){
+    int modAdd(int a,int b){
         a %= m;
         b %= m;
         return (a+b)%m;
@@ -15,17 +15,17 @@ private:
                 return 1;
             return 0;
         }
-        if(-1 != dp[sum][n])
-            return dp[sum][n];
-        long ans = 0;
+        if(-1 != dp[n][sum])
+            return dp[n][sum];
+        int ans = 0;
         for(int i=1;i<=k;i++){
             ans = modAdd(ans,helper(n-1,k,sum+i,target));
         }
-        return dp[sum][n] = ans;
+        return dp[n][sum] = ans;
     }
 public:
     int numRollsToTarget(int n, int k, int target) {
-        dp = vector<vector<int>>(target+1,vector<int>(n+1,-1));
+        dp = vector<vector<int>>(n+1,vector<int>(target+1,-1));
         return helper(n,k,0,target);
     }
 };
