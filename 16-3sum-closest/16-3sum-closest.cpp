3f211ff33eed36d0a4c56e5edaa756a42170a995
Dharmesh Poriya <84856389+Dharmesh-Poriya07@users.noreply.github.com>
2022-10-08 15:56:38 +0530

    Time: 720 ms (16.75%), Space: 16.4 MB (48.38%) - LeetHub

--- 16-3sum-closest/16-3sum-closest.cpp
+++ 16-3sum-closest/16-3sum-closest.cpp
@@ -4,7 +4,7 @@ public:
         sort(nums.begin(),nums.end());
         int threesum = 0, closest = INT_MAX;
         for(int i=0;i<nums.size();i++){
-            int low=0;
+            int low=i+1;
             int high = nums.size()-1;
             while(low<high){
                 if(i==low){
