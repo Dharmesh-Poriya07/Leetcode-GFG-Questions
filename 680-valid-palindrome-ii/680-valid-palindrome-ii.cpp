7abaf6e1c1890bfc62291343f976eab656120a66
Dharmesh Poriya <84856389+Dharmesh-Poriya07@users.noreply.github.com>
2022-10-09 16:58:33 +0530

    Time: 154 ms (19.07%), Space: 19.6 MB (88.49%) - LeetHub

--- 680-valid-palindrome-ii/680-valid-palindrome-ii.cpp
+++ 680-valid-palindrome-ii/680-valid-palindrome-ii.cpp
@@ -1,50 +1,30 @@
 class Solution {
-    bool h1(string &s,int low,int high){
-        int delcount = 0;
+    bool isPalindrome(string &s,int low,int high){
         while(low<high){
-            if(s[low]!=s[high]){
-                if(s[low+1]==s[high])
-                    low ++;
-                else if(s[high-1]==s[low])
-                    high --;
-                else 
-                    return false;
-                delcount++;
-                if(1<delcount) return false;
-            }else{
-                low ++;
-                high --;
-            }
-            
-        }
-        return true;
-    }
-    bool h2(string &s,int low,int high){
-        int delcount = 0;
-        while(low<high){
-            if(s[low]!=s[high]){
-                if(s[high-1]==s[low])
-                    high --;
-                else if(s[low+1]==s[high])
-                    low ++;
-                else 
-                    return false;
-                delcount++;
-                if(1<delcount) return false;
-            }else{
-                low ++;
-                high --;
-            }
-            
+            if(s[low] != s[high])
+                return false;
+            low++;
+            high--;
         }
         return true;
     }
 public:
     bool validPalindrome(string s) {
-        int delcount = 0;
         int low = 0,high=s.size()-1;
-        bool first = h1(s,low,high);
-        bool second = h2(s,low,high);
-        return first||second;
+        
+        while(low<high){
+            if(s[low]!=s[high]){
+                bool flag = false;
+                flag = isPalindrome(s,low,high-1);
+                if(!flag)
+                    return isPalindrome(s,low+1,high);
+                return flag;
+            }else{
+                low ++;
+                high --;
+            }
+            
+        }
+        return true;
     }
 };
