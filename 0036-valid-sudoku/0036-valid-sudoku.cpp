f0aff598f229f0546b4183b1bd166b4b4953063f
Dharmesh Poriya <84856389+Dharmesh-Poriya07@users.noreply.github.com>
2022-11-23 10:40:47 +0530

    Time: 35 ms (75.74%), Space: 22.3 MB (5.26%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.cpp
+++ 0036-valid-sudoku/0036-valid-sudoku.cpp
@@ -1,35 +1,21 @@
 class Solution {
 private:
-    bool isRowValid(vector<vector<char>>& board){
-        for(int i=0;i<9;i++){
-            unordered_map<char,int> m;
-            for(int j=0;j<9;j++){
-                if(0<m[board[i][j]]) return false;
-                if('0'<=board[i][j] and '9'>=board[i][j]) m[board[i][j]] ++;
-            }
-            if(0==m.size()) return false;
-        }
-        return true;
-    }
-    bool isColumnValid(vector<vector<char>>& board){
-        for(int j=0;j<9;j++){
-            unordered_map<char,int> m;
-            for(int i=0;i<9;i++){
-                if(0<m[board[i][j]]) return false;
-                if('0'<=board[i][j] and '9'>=board[i][j]) m[board[i][j]] ++;
-            }
-            if(0==m.size()) return false;
-        }
-        return true;
-    }
-    bool isSquareValid(vector<vector<char>>& board){
+    bool isValid(vector<vector<char>>& board){
+        vector<unordered_map<char,int>> rows(9);
+        vector<unordered_map<char,int>> columns(9);
         for(int i=0;i<9;i += 3){
             for(int j=0;j<9;j += 3){
                 unordered_map<char,int> m;
                 for(int x=i;x<i+3;x++){
                     for(int y=j;y<j+3;y++){
-                        if(0<m[board[x][y]]) return false;
-                        if('0'<=board[x][y] and '9'>=board[x][y]) m[board[x][y]] ++;
+                        if(0<m[board[x][y]] or 0<rows[x][board[x][y]] or 0<columns[y][board[x][y]]) return false;
+                        if('0'<=board[x][y] and '9'>=board[x][y]){
+                            m[board[x][y]] ++;
+                            rows[x][board[x][y]]++;
+                            columns[y][board[x][y]]++;
+                        }
+                        if(8==y and 0==rows[x].size()) return false;
+                        if(8==x and 0==columns[y].size()) return false;
                     }
                 }
                 if(0==m.size()) return false;
@@ -39,9 +25,6 @@ private:
     }
 public:
     bool isValidSudoku(vector<vector<char>>& board) {
-        bool checkrow = isRowValid(board);
-        bool checkcolumn = isColumnValid(board);
-        bool checksq = isSquareValid(board);
-        return (checkrow and checkcolumn and checksq);
+        return isValid(board);
     }
 };
