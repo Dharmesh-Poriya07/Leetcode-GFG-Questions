0baaefddf6da89a19a28414938f4b3152e232d8c
Dharmesh Poriya <84856389+Dharmesh-Poriya07@users.noreply.github.com>
2022-11-27 10:45:48 +0530

    Time: 2022 ms (24.93%), Space: 205.5 MB (32.31%) - LeetHub

--- 0446-arithmetic-slices-ii-subsequence/0446-arithmetic-slices-ii-subsequence.cpp
+++ 0446-arithmetic-slices-ii-subsequence/0446-arithmetic-slices-ii-subsequence.cpp
@@ -10,7 +10,7 @@ public:
                 if(0<dp[j][diff]){
                     ans += dp[j][diff];
                 }
-                dp[i][diff] = dp[i][diff]+dp[j][diff]+1;
+                dp[i][diff] += dp[j][diff]+1;
             }
         }
         return ans;
