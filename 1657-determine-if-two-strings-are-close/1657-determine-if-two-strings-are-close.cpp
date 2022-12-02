794b2863b5285459a7281fa5c045c8e5a044b6e4
Dharmesh Poriya <84856389+Dharmesh-Poriya07@users.noreply.github.com>
2022-12-02 19:46:43 +0530

    Time: 117 ms (87.43%), Space: 20.5 MB (82.93%) - LeetHub

--- 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.cpp
+++ 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.cpp
@@ -11,15 +11,12 @@ public:
         for(int i=0;i<26;i++){
             if(0==freq1[i] or freq1[i]==freq2[i]) continue;
             if(0==freq2[i]) return false;
-            bool flag = false;
             for(int j=i+1;j<26;j++){
                 if(freq2[j]==freq1[i]){
-                    flag = true;
                     swap(freq2[i],freq2[j]);
                     break;
                 }
             }
-            if(false==flag) return false;
         }
         for(int i=0;i<26;i++){
             if(freq1[i]!=freq2[i]) return false;
