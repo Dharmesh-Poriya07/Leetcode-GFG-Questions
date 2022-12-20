11ffda32f45134548903d312a69f6e9d28f0288e
Dharmesh Poriya <84856389+Dharmesh-Poriya07@users.noreply.github.com>
2022-12-20 21:44:15 +0530

    Added solution - LeetHub

---
+++ Clone a Binary Tree - GFG/clone-a-binary-tree.cpp
@@ -0,0 +1,155 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+/* A binary tree node has data, pointer to left child
+   and a pointer to right child */
+struct Node
+{
+	int data;
+	struct Node *left;
+	struct Node *right;
+	struct Node *random;
+	
+	Node(int x){
+	    data = x;
+	    left = NULL;
+	    right = NULL;
+	    random = NULL;
+	}
+};
+int checkcloned(Node* a, Node *b)
+{
+	if ((a == NULL and b == NULL))
+		return 1;
+
+	if (a != NULL && b != NULL)
+	{
+		int t = (a->data == b->data && checkcloned(a->left, b->left) && checkcloned(a->right, b->right));
+
+		if (a->random != NULL && b->random != NULL)
+			return (t && a->random->data == b->random->data);
+		if (a->random == b->random)
+			return t;
+
+		return 0;
+	}
+
+	return 0;
+
+}
+
+void inorder(Node *root)
+{
+    if (root == NULL)
+       return;
+    inorder(root->left);
+    cout << root->data << " ";
+    inorder(root->right);
+}
+
+// } Driver Code Ends
+/* A binary tree node has data, pointer to left child
+   and a pointer to right child 
+struct Node
+{
+    int data;
+    Node* left;
+    Node* right;
+    Node *random;
+};
+*/
+
+class Solution{
+    Node *newRoot;
+    Node *helper(Node *root){
+        if(!root) return NULL;
+        Node *newTree = new Node(root->data);
+        newTree->left = helper(root->left);
+        newTree->right = helper(root->right);
+        return newTree;
+    }
+    Node *findRandom(Node *nr,int x){
+        if(!nr) return NULL;
+        if(nr->data == x) return nr;
+        Node *left = findRandom(nr->left,x);
+        if(left) return left;
+        return findRandom(nr->right,x);
+    }
+    void setUpRandom(Node *root,Node *nroot){
+        if(!root) return;
+        if(root->random)
+            nroot->random = findRandom(newRoot,root->random->data);
+        setUpRandom(root->left,nroot->left);
+        setUpRandom(root->right,nroot->right);
+        return;
+    }
+  public:
+    
+    Node* cloneTree(Node* tree)
+    {
+       newRoot = helper(tree);
+       setUpRandom(tree,newRoot);
+       return newRoot;
+    }
+};
+
+
+//{ Driver Code Starts.
+/* Driver program to test size function*/
+int main()
+{
+  int t;
+  scanf("%d\n", &t);
+  while (t--)
+  {
+     map<int, Node*> m;
+     int n;
+     scanf("%d",&n);
+     struct Node *root = NULL;
+     struct Node *child;
+     while (n--)
+     {
+        Node *parent;
+        char lr;
+        int n1, n2;
+        scanf("%d %d %c", &n1, &n2, &lr);
+
+        if (m.find(n1) == m.end())
+        {
+           parent = new Node(n1);
+           m[n1] = parent;
+           if (root == NULL)
+             root = parent;
+        }
+        else
+           parent = m[n1];
+
+        child = new Node(n2);
+        if (lr == 'L'){
+          parent->left = child;
+           m[n2]  = child;
+        }
+        else if(lr=='R'){
+          parent->right = child;
+           m[n2]  = child;
+        }
+        else{
+          parent->random = m[n2];
+        }
+
+
+     }
+     Solution obj;
+     Node *t = obj.cloneTree(root);
+     if(t==root)
+        cout<<0<<endl;
+     else
+     cout<<checkcloned(root,t);
+     cout<<"\n";
+  }
+  return 0;
+}
+
+
+// } Driver Code Ends
