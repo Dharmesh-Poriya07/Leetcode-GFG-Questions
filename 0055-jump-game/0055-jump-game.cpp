958ee88182249ec0da8e8c88c987df9be03ccf53
Dharmesh Poriya <84856389+Dharmesh-Poriya07@users.noreply.github.com>
2022-12-26 18:23:32 +0530

    Time: 67 ms (92.48%), Space: 48.2 MB (94.13%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int req = 1;
+        for(int i = nums.size()-2;0<=i;i--){
+            if(nums[i]<req){
+                req++;
+            }else{
+                req = 1;
+            }
+        }
+        return req==1;
+    }
+};
