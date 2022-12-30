0f1aa48bd4e9166ee06005b8f4c9f41e8df1e01c
Dharmesh Poriya <84856389+Dharmesh-Poriya07@users.noreply.github.com>
2022-12-30 08:05:33 +0530

    Time: 15 ms (83.06%), Space: 12 MB (51.96%) - LeetHub

--- 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.cpp
+++ 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.cpp
@@ -3,16 +3,13 @@ private:
     vector<vector<int>> all_paths;
     vector<int> current_path;
     void dfs(vector<vector<int>> &graph,int src){
-        if(src == graph.size()-1){
-            current_path.push_back(src);
-            all_paths.push_back(current_path);
+        for(auto adj : graph[src]){
+            current_path.push_back(adj);
+            if(adj == graph.size()-1){
+                all_paths.push_back(current_path);
+            }else dfs(graph,adj);
             current_path.pop_back();
-            return ;
         }
-        current_path.push_back(src);
-        for(auto adj : graph[src])
-            dfs(graph,adj);
-        current_path.pop_back();
         return;
     }
 public:
@@ -20,6 +17,7 @@ public:
         int n = g.size();
         all_paths = vector<vector<int>>();
         current_path = vector<int>();
+        current_path.push_back(0);
         vector<vector<int>> graph(n);
         for(int i=0;i<n;i++)
             for(auto childs : g[i])
