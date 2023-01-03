252678bfdccfcba28bc2dd25ecf7a143e568f543
Dharmesh Poriya <84856389+Dharmesh-Poriya07@users.noreply.github.com>
2023-01-03 18:16:46 +0530

    Time: 150 ms (11.59%), Space: 22.8 MB (5.80%) - LeetHub

--- 0944-delete-columns-to-make-sorted/0944-delete-columns-to-make-sorted.cpp
+++ 0944-delete-columns-to-make-sorted/0944-delete-columns-to-make-sorted.cpp
@@ -4,19 +4,16 @@ public:
         vector<string> transpose(strs[0].size(),"");
         for(string row : strs){
             int i=0;
-            for(char ch : row){
+            for(char ch : row)
                 transpose[i++].push_back(ch);
-            }
         }
         int deleted_col = 0;
-        for(string col : transpose){
-            for(int i=1;i<col.size();i++){
+        for(string col : transpose)
+            for(int i=1;i<col.size();i++)
                 if(col[i-1]>col[i]){
                     deleted_col ++;
                     break;
                 }
-            }
-        }
         return deleted_col;
     }
 };
