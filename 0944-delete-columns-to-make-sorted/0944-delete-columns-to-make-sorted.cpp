f6db08cf0a53a8609d903bb7cd596232dff8994b
Dharmesh Poriya <84856389+Dharmesh-Poriya07@users.noreply.github.com>
2023-01-03 18:22:08 +0530

    Time: 72 ms (55.90%), Space: 12.2 MB (72.46%) - LeetHub

--- 0944-delete-columns-to-make-sorted/0944-delete-columns-to-make-sorted.cpp
+++ 0944-delete-columns-to-make-sorted/0944-delete-columns-to-make-sorted.cpp
@@ -1,16 +1,10 @@
 class Solution {
 public:
     int minDeletionSize(vector<string>& strs) {
-        vector<string> transpose(strs[0].size(),"");
-        for(string row : strs){
-            int i=0;
-            for(char ch : row)
-                transpose[i++].push_back(ch);
-        }
         int deleted_col = 0;
-        for(string col : transpose)
-            for(int i=1;i<col.size();i++)
-                if(col[i-1]>col[i]){
+        for(int col=0;col<strs[0].size();col++)
+            for(int row=1;row<strs.size();row++)
+                if(strs[row-1][col]>strs[row][col]){
                     deleted_col ++;
                     break;
                 }
