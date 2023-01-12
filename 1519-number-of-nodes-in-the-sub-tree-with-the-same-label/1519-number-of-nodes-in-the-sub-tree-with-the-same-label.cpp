6d3d712b4eb8b9a917fbf1d12d451602d8ee445c
Dharmesh Poriya <84856389+Dharmesh-Poriya07@users.noreply.github.com>
2023-01-12 18:37:08 +0530

    Time: 600 ms (82.96%), Space: 231.5 MB (30.37%) - LeetHub

--- 1519-number-of-nodes-in-the-sub-tree-with-the-same-label/1519-number-of-nodes-in-the-sub-tree-with-the-same-label.cpp
+++ 1519-number-of-nodes-in-the-sub-tree-with-the-same-label/1519-number-of-nodes-in-the-sub-tree-with-the-same-label.cpp
@@ -1,31 +1,26 @@
 class Solution {
 private:
     vector<int> ans;
-    vector<bool> visited;
-    vector<int> dfs(vector<vector<int>> &tree,int parent,string &labels){
+    vector<int> dfs(vector<vector<int>> &tree,int src,int parent,string &labels){
         vector<int> freq(26,0);
-        visited[parent] = true;
-        for(auto &adj : tree[parent]){
-            if(!visited[adj]){
-                int i=0;
-                for(auto &f : dfs(tree,adj,labels)) freq[i++]+=f;
-            }
-            
+        for(auto &adj : tree[src]){
+            if(adj == parent) continue;
+            int i=0;
+            for(auto &f : dfs(tree,adj,src,labels)) freq[i++]+=f;
         }
-        freq[labels[parent]-'a']++;
-        ans[parent] = freq[labels[parent]-'a'];
+        freq[labels[src]-'a']++;
+        ans[src] = freq[labels[src]-'a'];
         return freq;
     }
 public:
     vector<int> countSubTrees(int n, vector<vector<int>>& edges, string labels) {
         ans = vector<int>(n,0);
-        visited = vector<bool>(n,false);
         vector<vector<int>> tree(n);
         for(auto &e : edges){
             tree[e[0]].push_back(e[1]);
             tree[e[1]].push_back(e[0]);
         }
-        dfs(tree,0,labels);
+        dfs(tree,0,-1,labels);
         return ans;
     }
 };
