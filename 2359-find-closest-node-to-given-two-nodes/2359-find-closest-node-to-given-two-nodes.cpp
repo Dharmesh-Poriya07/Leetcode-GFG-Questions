424c9c5b88ecfeb7e54bbfa1edd9e69858cc1235
Dharmesh Poriya <84856389+Dharmesh-Poriya07@users.noreply.github.com>
2023-01-26 11:00:42 +0530

    Time: 227 ms (66.12%), Space: 100.6 MB (65.58%) - LeetHub

--- 2359-find-closest-node-to-given-two-nodes/2359-find-closest-node-to-given-two-nodes.cpp
+++ 2359-find-closest-node-to-given-two-nodes/2359-find-closest-node-to-given-two-nodes.cpp
@@ -1,38 +1,30 @@
 class Solution {
 private:
-    vector<int> findDistance(vector<vector<int>> &graph,int src){
-        vector<int> dist(graph.size(),INT_MAX);
-        queue<pair<int,int>> q;
-        q.push({src,0});
+    vector<int> findDistance(vector<int>& edges,int src){
+        vector<int> dist(edges.size(),INT_MAX);
+        queue<int> q;
+        q.push(src);
         dist[src] = 0;
-        vector<int> vis(graph.size(),false);
         while(!q.empty()){
-            auto curr = q.front();q.pop();
-            vis[curr.first] = true;
-            for(int child : graph[curr.first]){
-                if(!vis[child]){
-                    dist[child] = curr.second+1;
-                    q.push({child,dist[child]});
-                }else{
-                    // this else condition not required for given question,
-                    // if we have nodes with more than one edge than this condition is required for finding smallest dstance.
-                    dist[child] = min(dist[child],curr.second+1);
-                }
+            int parent = q.front();q.pop();
+            int child = edges[parent];
+            if(child!=-1 and dist[child]==INT_MAX){
+                dist[child] = dist[parent]+1;
+                q.push(child);
             }
+            // else{
+            //     // this else condition not required for given question,
+            //     // if we have nodes with more than one edge than this condition is required for finding smallest dstance.
+            //     dist[child] = min(dist[child],curr.second+1);
+            // }
         }
         return dist;
     }
 public:
     int closestMeetingNode(vector<int>& edges, int node1, int node2) {
         int n = edges.size();
-        vector<vector<int>> graph(n);
-        for(int i=0;i<n;i++){
-            if(-1!=edges[i]){
-                graph[i].push_back(edges[i]);
-            }
-        }
-        auto dist1 = findDistance(graph,node1);
-        auto dist2 = findDistance(graph,node2);
+        auto dist1 = findDistance(edges,node1);
+        auto dist2 = findDistance(edges,node2);
         int mndist = INT_MAX;
         int node;
         for(int i=0;i<n;i++){
