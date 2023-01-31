36694e10ff5e68bc81ef35c55d92e96c53cfdf5c
Dharmesh Poriya <84856389+Dharmesh-Poriya07@users.noreply.github.com>
2023-01-31 20:40:38 +0530

    Time: 756 ms (8.76%), Space: 137.2 MB (7.14%) - LeetHub

---
+++ 1626-best-team-with-no-conflicts/1626-best-team-with-no-conflicts.cpp
@@ -0,0 +1,25 @@
+class Solution {
+private:
+    vector<vector<int>> dp;
+    int helper(vector<vector<int>> &pr,int ind,int prev){
+        if(ind==pr.size()) return 0;
+        if(-1==prev){
+            return dp[ind+1][prev+1] = max(pr[ind][1]+helper(pr,ind+1,ind),helper(pr,ind+1,prev));
+        }
+        if(-1!=dp[ind+1][prev+1]) return dp[ind+1][prev+1];
+        int op1=-1,op2 = -1;
+        if(-1!=prev and ((pr[prev][0]==pr[ind][0] and pr[ind][1]>=pr[prev][1]) or (pr[ind][1]<=pr[prev][1])))
+            op1 = pr[ind][1] + helper(pr,ind+1,ind);
+        op2 = helper(pr,ind+1,prev);
+        return dp[ind+1][prev+1]  = max(op1,op2);
+    }
+public:
+    int bestTeamScore(vector<int>& scores, vector<int>& ages) {
+        vector<vector<int>> pr;
+        dp = vector<vector<int>>(scores.size()+1,vector<int>(scores.size()+1,-1));
+        for(int i=0;i<scores.size();i++)
+            pr.push_back({ages[i],scores[i]});
+        sort(pr.rbegin(),pr.rend());
+        return helper(pr,0,-1);
+    }
+};
