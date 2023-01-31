a1e5710a5763c7e68baab99bbc9628f276bc4bb1
Dharmesh Poriya <84856389+Dharmesh-Poriya07@users.noreply.github.com>
2023-01-31 20:52:54 +0530

    Time: 795 ms (7.79%), Space: 138.7 MB (6.17%) - LeetHub

--- 1626-best-team-with-no-conflicts/1626-best-team-with-no-conflicts.cpp
+++ 1626-best-team-with-no-conflicts/1626-best-team-with-no-conflicts.cpp
@@ -4,14 +4,14 @@ private:
     int helper(vector<vector<int>> &pr,int ind,int prev){
         if(ind==pr.size()) return 0;
         if(-1==prev){
-            return dp[ind+1][prev+1] = max(pr[ind][1]+helper(pr,ind+1,ind),helper(pr,ind+1,prev));
+            return dp[ind][prev+1] = max(pr[ind][1]+helper(pr,ind+1,ind),helper(pr,ind+1,prev));
         }
-        if(-1!=dp[ind+1][prev+1]) return dp[ind+1][prev+1];
+        if(-1!=dp[ind][prev+1]) return dp[ind][prev+1];
         int op1=-1,op2 = -1;
-        if(-1!=prev and ((pr[prev][0]==pr[ind][0]) or (pr[ind][1]<=pr[prev][1])))
+        if(-1!=prev and (pr[prev][0]==pr[ind][0] or pr[ind][1]<=pr[prev][1]))
             op1 = pr[ind][1] + helper(pr,ind+1,ind);
         op2 = helper(pr,ind+1,prev);
-        return dp[ind+1][prev+1]  = max(op1,op2);
+        return dp[ind][prev+1]  = max(op1,op2);
     }
 public:
     int bestTeamScore(vector<int>& scores, vector<int>& ages) {
@@ -20,6 +20,9 @@ public:
         for(int i=0;i<scores.size();i++)
             pr.push_back({ages[i],scores[i]});
         sort(pr.rbegin(),pr.rend());
+        for(auto v : pr){
+            cout<<v[0]<<" "<<v[1]<<endl;
+        }
         return helper(pr,0,-1);
     }
 };
