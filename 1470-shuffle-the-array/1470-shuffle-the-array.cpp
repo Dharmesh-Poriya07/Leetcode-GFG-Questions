83ad33ca15c8bb5fbe3de5574d52fc17dbffec14
Dharmesh Poriya <84856389+Dharmesh-Poriya07@users.noreply.github.com>
2023-02-06 18:23:50 +0530

    Time: 0 ms (100.00%), Space: 9.6 MB (87.13%) - LeetHub

---
+++ 1470-shuffle-the-array/1470-shuffle-the-array.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<int> shuffle(vector<int>& nums, int n) {
+        vector<int> ans(2*n);
+        int i=0,j=n;
+        for(int k=0;k<2*n;k++){
+            if(0==k%2)
+                ans[k] = nums[i++];
+            else
+                ans[k] = nums[j++];
+        }
+        return ans;
+    }
+};
