322ec9e650272a664d566cf7dde04131ffd21e00
Dharmesh Poriya <84856389+Dharmesh-Poriya07@users.noreply.github.com>
2023-02-08 19:37:28 +0530

    Time: 524 ms (20.33%), Space: 17.7 MB (15.91%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.cpp
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -2,15 +2,18 @@ class Solution {
 public:
     int jump(vector<int>& nums) {
         int n = nums.size();
-        vector<long> dp(n,INT_MAX);
+        vector<long> dp(n,-1);
         dp[n-1] = 0;
         for(int i=n-2;0<=i;i--){
             if(0<nums[i]){
                 if(nums[i]>=n-i-1)
                     dp[i] = 1;
                 else{
+                    dp[i] = INT_MAX;
                     for(int j=1;j<=nums[i];j++){
-                        dp[i] = min(dp[i],1+min(dp[i+1]!=-1?dp[i+1]:INT_MAX-1,dp[i+j]!=-1?dp[i+j]:INT_MAX-1));
+                        if(-1!=dp[i+j]){
+                            dp[i] = min(dp[i],1+dp[i+j]);
+                        }
                     }
                 }
             }else{
