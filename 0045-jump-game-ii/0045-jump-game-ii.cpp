40ec7154e4268469823b3dd03d4aff86a3bd0138
Dharmesh Poriya <84856389+Dharmesh-Poriya07@users.noreply.github.com>
2023-02-08 19:38:24 +0530

    Time: 486 ms (23.18%), Space: 17.6 MB (17.45%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.cpp
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -6,19 +6,14 @@ public:
         dp[n-1] = 0;
         for(int i=n-2;0<=i;i--){
             if(0<nums[i]){
-                if(nums[i]>=n-i-1)
-                    dp[i] = 1;
-                else{
-                    dp[i] = INT_MAX;
-                    for(int j=1;j<=nums[i];j++){
-                        if(-1!=dp[i+j]){
+                dp[i] = INT_MAX;
+                if(nums[i]>=n-i-1) dp[i] = 1;
+                else
+                    for(int j=1;j<=nums[i];j++)
+                        if(-1!=dp[i+j])
                             dp[i] = min(dp[i],1+dp[i+j]);
-                        }
-                    }
-                }
-            }else{
+            }else
                 dp[i] = -1;
-            }
         }
         return dp[0];
     }
