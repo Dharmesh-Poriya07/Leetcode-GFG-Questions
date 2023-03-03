8a32665adbf2de745e541002f516cf378b70dbe2
Dharmesh Poriya <84856389+Dharmesh-Poriya07@users.noreply.github.com>
2023-03-03 14:38:26 +0530

    Time: 1148 ms (7.66%), Space: 252.7 MB (11.26%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.cpp
+++ 0912-sort-an-array/0912-sort-an-array.cpp
@@ -1,6 +1,6 @@
 class Solution {
 private:
-    void merge(vector<int> &nums,int left,int mid,int right){
+    void mergeTwoSortedArray(vector<int> &nums,int left,int mid,int right){
         vector<int> left_arr,right_arr;
         
         for(int i=left;i<=mid;i++) left_arr.push_back(nums[i]);
@@ -24,14 +24,11 @@ private:
             int mid = left+(right-left)/2;
             mergeSort(nums,left,mid);
             mergeSort(nums,mid+1,right);
-            merge(nums,left,mid,right);
+            mergeTwoSortedArray(nums,left,mid,right);
         }
     }
 public:
     vector<int> sortArray(vector<int>& nums) {
-        for(int i=0,j=277;i<=2*1e4;i++,j++){
-            cout<<j<<",";
-        }
         mergeSort(nums,0,nums.size()-1);
         return nums;
     }
