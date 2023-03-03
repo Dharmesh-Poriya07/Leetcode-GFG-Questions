efe64f2d229fe4eaf29c85751378ffd821d1a7e8
Dharmesh Poriya <84856389+Dharmesh-Poriya07@users.noreply.github.com>
2023-03-03 14:37:02 +0530

    Time: 1410 ms (5.42%), Space: 252.7 MB (11.26%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.cpp
+++ 0912-sort-an-array/0912-sort-an-array.cpp
@@ -1,28 +1,23 @@
 class Solution {
 private:
     void merge(vector<int> &nums,int left,int mid,int right){
-        vector<int> left_arr(mid-left+1),right_arr(right-mid);
-        vector<int> arr(right-left+1);
-        for(int i=left,j=0;i<=mid;i++,j++) left_arr[j] = nums[i];
-        for(int i=mid+1,j=0;i<=right;i++,j++) right_arr[j] = nums[i];
+        vector<int> left_arr,right_arr;
+        
+        for(int i=left;i<=mid;i++) left_arr.push_back(nums[i]);
+        for(int i=mid+1;i<=right;i++) right_arr.push_back(nums[i]);
         
         int i,j,k;
-        i = j = k = 0;
-        while(i<left_arr.size() && j<right_arr.size()){
-            if(left_arr[i]<=right_arr[j]){
-                arr[k] = left_arr[i];
-                i++;
-            }else{
-                arr[k] = right_arr[j];
-                j++;
-            }
+        i = j = 0;
+        k = left;
+        
+        while(i<left_arr.size() and j<right_arr.size()){
+            if(left_arr[i]<=right_arr[j]) nums[k] = left_arr[i++];
+            else nums[k] = right_arr[j++];
             k++;
         }
-        while(i<left_arr.size()) arr[k++] = left_arr[i++];
-        while(j<right_arr.size()) arr[k++] = right_arr[j++];
         
-        k=0;
-        while(k<arr.size()) nums[left++] = arr[k++];
+        while(i<left_arr.size()) nums[k++] = left_arr[i++];
+        while(j<right_arr.size()) nums[k++] = right_arr[j++];
     }
     void mergeSort(vector<int> &nums,int left,int right){
         if(left<right){
