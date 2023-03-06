38abca9761c9f3f3f501bf7155eac430f7723703
Dharmesh Poriya <84856389+Dharmesh-Poriya07@users.noreply.github.com>
2023-03-06 15:45:24 +0530

    Time: 129 ms (22.77%), Space: 70.3 MB (83.30%) - LeetHub

--- 2444-count-subarrays-with-fixed-bounds/2444-count-subarrays-with-fixed-bounds.cpp
+++ 2444-count-subarrays-with-fixed-bounds/2444-count-subarrays-with-fixed-bounds.cpp
@@ -1,13 +1,22 @@
 class Solution {
 public:
     long long countSubarrays(vector<int>& nums, int minK, int maxK) {
-        long ans = 0, jbad = -1, jmin = -1, jmax = -1, n = nums.size();
-        for (int i = 0; i < n; ++i) {
-            if (nums[i] < minK || nums[i] > maxK) jbad = i;
-            if (nums[i] == minK) jmin = i;
-            if (nums[i] == maxK) jmax = i;
-            ans = ans + max(0L, min(jmin, jmax) - jbad);
+        int n = nums.size();
+        int last_invalid_index, last_min_k, last_max_k;
+        last_invalid_index = last_min_k = last_max_k = -1;
+        
+        long long count = 0;
+        for(int i=0;i<n;i++){
+            if(nums[i] >= minK and nums[i] <= maxK){
+                if(nums[i]==minK) last_min_k = i;
+                if(nums[i]==maxK) last_max_k = i;
+                
+                count += max(0,min(last_min_k,last_max_k)-last_invalid_index);
+            }else{
+                last_invalid_index = i;
+                last_min_k = last_max_k = -1;
+            }
         }
-        return ans;
+        return count;
     }
 };
