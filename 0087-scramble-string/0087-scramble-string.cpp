e93f37430bdb4561d7d8f807f04e1f3786de78cb
Dharmesh Poriya <84856389+Dharmesh-Poriya07@users.noreply.github.com>
2023-03-30 11:12:54 +0530

    Time: 175 ms (40.22%), Space: 34.7 MB (48.85%) - LeetHub

---
+++ 0087-scramble-string/0087-scramble-string.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    unordered_map<string, bool> mp;
+    
+    bool solve(string a, string b)
+    {
+        if(a.compare(b) == 0) return true;
+        
+        if(a.length() <= 1) return false;  //1character can't be compared
+        
+        int n = a.length();
+        bool check = false;
+        
+        string key = a;
+        
+        key += ' ' + b;
+        
+        if(mp.find(key) != mp.end()) 
+            return mp[key];
+        
+        for(int i = 1; i < n; i++)
+        {
+           bool unswap = solve(a.substr(0,i), b.substr(0,i)) and solve(a.substr(i,n - i), b.substr(i,n - i));
+           bool swap = solve(a.substr(0,i), b.substr(n - i,i)) and solve(a.substr(i), b.substr(0, n - i));
+            
+           if(swap || unswap){
+               check = true;
+               break;
+           }
+        }
+        
+        return mp[key] =  check;
+    }
+    
+    bool isScramble(string s1, string s2) 
+    {
+        if(s1 == s2) return true;
+        
+        if(s1.length() != s2.length()) return false;
+        
+        if(s1.empty() and s2.empty()) return true;
+        
+        return solve(s1,s2);
+    }
+};
