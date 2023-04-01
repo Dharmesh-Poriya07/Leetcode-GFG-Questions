35d6e300d9bdd978aeff46db3ab609cf9a4f3e8e
Dharmesh Poriya <84856389+Dharmesh-Poriya07@users.noreply.github.com>
2023-04-01 16:55:12 +0530

    Time: 26 ms (77.49%), Space: 8.1 MB (64.92%) - LeetHub

--- 1444-number-of-ways-of-cutting-a-pizza/1444-number-of-ways-of-cutting-a-pizza.cpp
+++ 1444-number-of-ways-of-cutting-a-pizza/1444-number-of-ways-of-cutting-a-pizza.cpp
@@ -1,41 +1,39 @@
-static int MOD=1e9+7;
 class Solution {
 public:
-    vector<vector<vector<int>>> memo;
-    vector<vector<int>> prefixSum;
-    int getApples(int row1,int col1,int row2,int col2) 
+    int ways(vector<string>& pizza, int k) {
+        int rows = pizza.size();
+        int cols = pizza[0].size();
         
-    {
-        return prefixSum[row2+1][col2+1]-prefixSum[row1][col2+1]-prefixSum[row2+1][col1]+prefixSum[row1][col1];
+        vector<vector<int>> presum(rows+1,vector<int>(cols+1,0));
+        
+        for(int i=rows-1;0<=i;i--){
+            for(int j=cols-1;0<=j;j--){
+                presum[i][j] = presum[i][j+1]+presum[i+1][j]-presum[i+1][j+1]+('A'==pizza[i][j]?1:0);
+            }
+        }
+        vector<vector<vector<int>>> dp(k+1,vector<vector<int>>(rows+1,vector<int>(cols+1,-1)));
+        // i,j and k will change so we need 3D DP.
+        return solve(rows,cols,k-1,0,0,presum,dp);
     }
-    int dp(vector<string>& pizza,int left,int top,int k)
-    {
-        if(memo[top][left][k]!=-1)
-            return memo[top][left][k];
-        if(k==1)
-            return getApples(top,left,pizza.size()-1,pizza[0].size()-1)!=0;    
+    
+    int solve(int rows,int cols,int k,int row,int col,vector<vector<int>> &presum,vector<vector<vector<int>>> &dp){
+        if(0==presum[row][col]) return 0;
+        if(0==k) return 1;
+        if(-1 != dp[k][row][col]) return dp[k][row][col];
         
-        memo[top][left][k]=0;
-        for(int c=left;c<pizza[0].size()-1;c++)    
-            
-            if(getApples(top,left,pizza.size()-1,c))    
-                
-               memo[top][left][k]=(memo[top][left][k]+dp(pizza,c+1,top,k-1))%MOD;  
-        for(int r=top;r<pizza.size()-1;r++)        
-            
-            if(getApples(top,left,r,pizza[0].size()-1)) 
-                
-                memo[top][left][k]=(memo[top][left][k]+dp(pizza,left,r+1,k-1))%MOD;
-        return memo[top][left][k];
-    }
-    int ways(vector<string>& pizza, int k) 
-    {
-        memo.resize(pizza.size(),vector<vector<int>>(pizza[0].size(),vector<int>(k+1,-1)));
-        prefixSum.resize(pizza.size()+1,vector<int>(pizza[0].size()+1,0));      
+        int ans = 0;
         
-        for(int i=0;i<pizza.size();i++)
-            for(int j=0;j<pizza[0].size();j++)
-                prefixSum[i+1][j+1]=prefixSum[i+1][j]+prefixSum[i][j+1]-prefixSum[i][j]+(pizza[i][j]=='A');
-        return dp(pizza,0,0,k);
+        // cutting horizontal
+        for(int r=row+1;r<rows;r++){
+            if(0<presum[row][col]-presum[r][col])
+                ans = (ans+solve(rows,cols,k-1,r,col,presum,dp))%1000000007;
+        }
+        
+        for(int c=col+1;c<cols;c++){
+            if(0<presum[row][col]-presum[row][c])
+                ans = (ans+solve(rows,cols,k-1,row,c,presum,dp))%1000000007;
+        }
+        
+        return dp[k][row][col] = ans;
     }
 };
