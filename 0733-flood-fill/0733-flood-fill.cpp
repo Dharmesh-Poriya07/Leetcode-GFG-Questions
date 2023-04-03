8893eaf447a0f9feae826f2610f01da560f374b8
Dharmesh Poriya <84856389+Dharmesh-Poriya07@users.noreply.github.com>
2023-04-03 22:34:08 +0530

    Time: 12 ms (36.95%), Space: 14.4 MB (18.44%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -5,14 +5,15 @@ public:
         queue<pair<int,int>> q;
         q.push({sr,sc});
         int curr_pixel = image[sr][sc];
+        image[sr][sc] = color;
         vector<vector<int>> dir = {{-1,0},{0,1},{1,0},{0,-1}};
         while(!q.empty()){
             auto curr = q.front(); q.pop();
-            image[curr.first][curr.second] = color;
             for(auto d : dir){
                 int r = curr.first+d[0];
                 int c = curr.second+d[1];
                 if(image.size()>r and image[0].size()>c and -1<r and -1<c and image[r][c]==curr_pixel){
+                    image[r][c] = color;
                     q.push({r,c});
                 }
             }
