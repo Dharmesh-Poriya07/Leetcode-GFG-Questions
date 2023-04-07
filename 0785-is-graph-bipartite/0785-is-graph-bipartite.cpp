2bc4c94ef4bfdb18011c00967467858ae2d25cf4
Dharmesh Poriya <84856389+Dharmesh-Poriya07@users.noreply.github.com>
2023-04-07 23:43:04 +0530

    Time: 23 ms (80.34%), Space: 14.1 MB (16.95%) - LeetHub

--- 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
@@ -1,27 +1,28 @@
 class Solution {
 public:
-    bool isBipartite(vector<vector<int>>& graph) {
-        int n = graph.size();
-        vector<int> vis(n,0);
-        for(int i=0;i<n;i++){
-            if(0==vis[i]){
-                queue<vector<int>> q;
-                q.push({i,1});
-                vis[i] = 1;
-                while(!q.empty()){
-                    auto curr = q.front(); q.pop();
-                    for(int adj : graph[curr[0]]){
-                        if(0==vis[adj]){
-                            q.push({adj,1==curr[1]?2:1});
-                            vis[adj] = 1==curr[1]?2:1;
-                        }else{
-                            if(vis[adj]==curr[1])
-                                return false;
-                        }
-                    }
+    bool bfs(vector<vector<int>>& graph,vector<int> &vis,int src){
+        queue<vector<int>> q;
+        q.push({src,1});
+        vis[src] = 1;
+        while(!q.empty()){
+            auto curr = q.front(); q.pop();
+            for(int adj : graph[curr[0]]){
+                if(vis[adj]==curr[1]) return false;
+                if(0==vis[adj]){
+                    q.push({adj,1==curr[1]?2:1});
+                    vis[adj] = 1==curr[1]?2:1;
                 }
             }
         }
+        return true;
+    }
+    bool isBipartite(vector<vector<int>>& graph) {
+        int n = graph.size();
+        vector<int> vis(n,0);
+        for(int i=0;i<n;i++)
+            if(0==vis[i])
+                if(!bfs(graph,vis,i)) return false;
+        
         return true;
     }
 };
