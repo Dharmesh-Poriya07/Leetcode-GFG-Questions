632851266ae5744d8fc27e1ff4cb90d7d87b626d
Dharmesh Poriya <84856389+Dharmesh-Poriya07@users.noreply.github.com>
2023-04-09 19:58:30 +0530

    Time: 892 ms (14.53%), Space: 195.9 MB (32.90%) - LeetHub

---
+++ 1857-largest-color-value-in-a-directed-graph/1857-largest-color-value-in-a-directed-graph.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    int largestPathValue(string colors, vector<vector<int>>& edges) {
+        int n = colors.size();
+        vector<int> g[n];
+        unordered_map<int,int> indegree;
+        for(auto e : edges){
+            g[e[0]].push_back(e[1]);
+            indegree[e[1]]++;
+        }
+        
+        queue<int> q;
+        vector<vector<int>> dp(n+1,vector<int>(26,0));
+        int visited = 0, ans = 0;
+        
+        for(int i=0;i<n;i++){
+            if(0==indegree[i]){
+                q.push(i);
+                dp[i][colors[i]-'a'] = 1;
+            }
+        }
+        
+        while(!q.empty()){
+            int u = q.front(); q.pop();
+            visited++;
+            int cmax = *max_element(begin(dp[u]),end(dp[u]));
+            ans = max(ans,cmax);
+            for(int v : g[u]){
+                for(int color=0;color<26;color++){
+                    dp[v][color] = max(dp[v][color],dp[u][color]+(color==colors[v]-'a'));
+                }
+                indegree[v]--;
+                if(0==indegree[v])
+                    q.push(v);
+            }
+        }
+        // visited==n? there is no cycle : cycle present
+        if(visited<n){
+            return -1;
+        }
+        
+        
+        return ans;
+    }
+};
