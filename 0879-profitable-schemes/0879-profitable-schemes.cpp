2943ecfadc1b3dee8bba92e627ec095fb750f475
Dharmesh Poriya <84856389+Dharmesh-Poriya07@users.noreply.github.com>
2023-04-21 14:15:34 +0530

    Time: 203 ms (51.41%), Space: 12.9 MB (66.51%) - LeetHub

---
+++ 0879-profitable-schemes/0879-profitable-schemes.cpp
@@ -0,0 +1,27 @@
+class Solution {
+    int mod = 1e9+7;
+    int dp[101][101][101];
+    
+    int solve(int index,int selected_members,int curr_profit,int n,int minProfit,vector<int>& group, vector<int>& profit){
+        if(group.size()<=index){
+            return curr_profit >= minProfit;
+        }
+        
+        if(-1 != dp[index][selected_members][curr_profit]) return dp[index][selected_members][curr_profit];
+        
+        int not_selected = solve(index+1,selected_members,curr_profit,n,minProfit,group,profit);
+        int selected = 0;
+        
+        if(selected_members+group[index] <= n){
+            selected = solve(index+1,selected_members+group[index],min(minProfit,profit[index]+curr_profit),n,minProfit,group,profit);
+        }
+        
+        return dp[index][selected_members][curr_profit] = (selected%mod + not_selected%mod)%mod;
+    }
+public:
+    int profitableSchemes(int n, int minProfit, vector<int>& group, vector<int>& profit) {
+        memset(dp,-1,sizeof(dp));
+        // first 3 args of solve function are : current_index,current_selected_members,current_profit
+        return solve(0,0,0,n,minProfit,group,profit);
+    }
+};
