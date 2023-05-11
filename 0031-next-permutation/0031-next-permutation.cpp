c0a67b31927eb0628505d77537356a32273093c6
Dharmesh Poriya <84856389+Dharmesh-Poriya07@users.noreply.github.com>
2023-05-11 15:25:22 +0530

    Time: 8 ms (23.88%), Space: 12.1 MB (36.90%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -1,7 +1,29 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        next_permutation(nums.begin(),nums.end());
+        int ind = -1; // it's index where we find there exist greater element in [ind+1,n] range.
+        int n = nums.size();
+        
+        for(int i=n-2;i>=0;i--){
+            if(nums[i]<nums[i+1]){
+                ind = i;
+                break;
+            }
+        }
+        
+        if(-1 == ind){
+            reverse(begin(nums),end(nums));
+            return;
+        }
+        
+        for(int i=n-1;i>ind;i--){
+            if(nums[i] > nums[ind]){
+                swap(nums[i],nums[ind]);
+                break;
+            }
+        }
+        
+        reverse(begin(nums)+ind+1,end(nums));
         return;
     }
 };
