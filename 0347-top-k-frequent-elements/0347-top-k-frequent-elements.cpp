69c9bbcee3689f0ac99c5532c38ab3568338dab8
Dharmesh Poriya <84856389+Dharmesh-Poriya07@users.noreply.github.com>
2023-05-22 10:25:48 +0530

    Time: 17 ms (61.19%), Space: 15.4 MB (10.22%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -4,13 +4,20 @@ public:
         unordered_map<int,int> freq;
         for(auto &n : nums) freq[n]++;
         
-        priority_queue<pair<int,int>> pq;
-        for(auto [key,val] : freq) pq.push({val,key});
+        vector<vector<int>> bucket(nums.size()+1);
+        for(auto [key,val] : freq){
+            bucket[val].push_back(key);
+        }
         
         vector<int> ans;
-        while(k--){
-            ans.push_back(pq.top().second);
-            pq.pop();
+        for(int i=nums.size();i>=0 and k>0;i--){
+            if(0 < bucket[i].size()){
+                while(bucket[i].size()>0){
+                    ans.push_back(bucket[i].back());
+                    bucket[i].pop_back();
+                    k--;
+                }
+            }
         }
         
         return ans;
