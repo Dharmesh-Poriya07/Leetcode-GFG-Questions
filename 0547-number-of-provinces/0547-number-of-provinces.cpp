979e4840264d6a73de64015e4181d01a8eb39de1
Dharmesh Poriya <84856389+Dharmesh-Poriya07@users.noreply.github.com>
2023-06-02 15:25:41 +0530

    Time: 22 ms (86.75%), Space: 13.8 MB (71.47%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,23 +1,73 @@
+class DisjointSet{
+    vector<int> parent,rank,size;
+    public:
+    DisjointSet(int n){
+        parent.resize(n+1);
+        rank.resize(n+1);
+        size.resize(n+1);
+        for(int i=0;i<=n;i++){
+           parent[i] = i; 
+           rank[i] = 0;
+           size[i] = 1;
+        } 
+    }
+    
+    int findParent(int x){
+        if(x == parent[x]) return x;
+        return parent[x] = findParent(parent[x]);
+    }
+    
+    void unionByRank(int u,int v){
+        int pu = findParent(u);
+        int pv = findParent(v);
+        
+        if(pu==pv) return;
+        
+        if(rank[pu] < rank[pv]) parent[pu] = pv;
+        else if(rank[pv] < rank[pu]) parent[pv] = pu;
+        else{
+            parent[pv] = pu;
+            rank[pu]++;
+        }
+    }
+    
+    void unionBySize(int u,int v){
+        int pu = findParent(u);
+        int pv = findParent(v);
+        
+        if(pu == pv) return;
+        
+        if(size[pu] < size[pv]){
+            parent[pu] = pv;
+            size[pv] += size[pu];
+        }else{
+            parent[pv] = pu;
+            size[pu] += size[pv];
+        }
+    }
+
+    int findNoOfParent(){
+        int cnt = 0;
+        for(int i=0;i<parent.size();i++)
+            if(i==parent[i])
+                cnt ++;
+        return cnt;
+    }
+};
+
 class Solution {
 public:
-    int findCircleNum(vector<vector<int>>& adj) {
-        int n = adj.size();
-        int province = 0;
+    int findCircleNum(vector<vector<int>>& isConnected) {
+        int n = isConnected.size();
+        DisjointSet *dsu = new DisjointSet(n);
+
         for(int i=0;i<n;i++){
-            if(1&adj[i][i]){
-                dfs(adj,i);
-                province ++;
+            for(int j=0;j<=i;j++){
+                if(1==isConnected[i][j])dsu->unionByRank(i,j);
             }
         }
-        return province;
-    }
-    void dfs(vector<vector<int>> &adj,int src){
-        adj[src][src] = 0;
-        auto adjs = adj[src];
-        for(int i=0;i<adjs.size();i++){
-            if(0!=adjs[i] && 1==adj[i][i]){
-                dfs(adj,i);
-            }
-        }
+
+        int cnt = dsu->findNoOfParent();
+        return cnt-1;
     }
 };
