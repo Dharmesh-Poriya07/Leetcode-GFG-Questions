e487bd6170eee1b18a1f9f9ed79b3d483032a9f5
Dharmesh Poriya <84856389+Dharmesh-Poriya07@users.noreply.github.com>
2023-06-03 11:06:15 +0530

    Time: 347 ms (40.68%), Space: 117.6 MB (69.14%) - LeetHub

---
+++ 1492-time-needed-to-inform-all-employees/1492-time-needed-to-inform-all-employees.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int numOfMinutes(int n, int headID, vector<int>& manager, vector<int>& informTime) {
+        vector<int> adj[n];
+        for(int i=0;i<n;i++){
+            if(-1 != manager[i]) adj[manager[i]].push_back(i);
+        }
+
+        queue<pair<int,int>> q;
+        q.push({headID,informTime[headID]});
+        int mxtime = 0;
+
+        while(!q.empty()){
+            auto curr = q.front(); q.pop();
+            mxtime = max(mxtime,curr.second);
+            for(auto v : adj[curr.first]){
+                q.push({v,curr.second+informTime[v]});
+            }
+        }
+        return mxtime;
+    }
+};
