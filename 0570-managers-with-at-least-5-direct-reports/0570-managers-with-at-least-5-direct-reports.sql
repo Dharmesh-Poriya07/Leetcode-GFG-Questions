a1a31928d673cf80b5d0b288583fc9a76a79e4fa
Dharmesh Poriya <84856389+Dharmesh-Poriya07@users.noreply.github.com>
2023-06-12 11:07:06 +0530

    Time: 594 ms (32.37%), Space: 0B (100%) - LeetHub

--- 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
+++ 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
@@ -1,2 +1,5 @@
+# using subquery
+# select name from employee where id in (select managerId from employee group by managerId having count(name)>=5);
 
-select name from employee where id in (select managerId from employee group by managerId having count(name)>=5);
+select e1.name from employee e1 join employee e2 on 
+e1.id = e2.managerId group by e1.id having count(e1.id)>=5;
