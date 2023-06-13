2f8aee8126373c1a2e4c54f7062455e5e7f7b4cc
Dharmesh Poriya <84856389+Dharmesh-Poriya07@users.noreply.github.com>
2023-06-13 09:42:30 +0530

    Time: 247 ms (41.4%), Space: 44.1 MB (19.47%) - LeetHub

--- 2428-equal-row-and-column-pairs/2428-equal-row-and-column-pairs.cpp
+++ 2428-equal-row-and-column-pairs/2428-equal-row-and-column-pairs.cpp
@@ -3,17 +3,24 @@ public:
     int equalPairs(vector<vector<int>>& grid) {
         int n = grid.size();
         int ans = 0;
+        unordered_map<string,int> freq;
         for(int i=0;i<n;i++){
+            string row = "";
             for(int j=0;j<n;j++){
-                bool flag = false;
-                for(int ki=0,kj=0;ki<n;ki++,kj++){
-                    if(grid[ki][j] != grid[i][kj]){
-                        flag = true;
-                        break;
-                    }
-                }
-                if(!flag) ans++;
+                for(char ch : to_string(grid[i][j]))
+                    row.push_back(ch);
+                row.push_back('#');
             }
+            freq[row]++;
+        }
+        for(int j=0;j<n;j++){
+            string col = "";
+            for(int i=0;i<n;i++){
+                for(char ch : to_string(grid[i][j]))
+                    col.push_back(ch);
+                col.push_back('#');
+            }
+            ans += freq[col];
         }
         return ans;
     }
