5069ea10f41fb46933ea150e8e49c7a40e610689
Dharmesh Poriya <84856389+Dharmesh-Poriya07@users.noreply.github.com>
2023-06-14 15:53:41 +0530

    Time: 1366 ms (59.16%), Space: 0B (100%) - LeetHub

--- 1664-find-users-with-valid-e-mails/1664-find-users-with-valid-e-mails.sql
+++ 1664-find-users-with-valid-e-mails/1664-find-users-with-valid-e-mails.sql
@@ -1,3 +1,11 @@
 select *
 from users
 where mail regexp '^[a-zA-Z]+[a-zA-Z0-9_.-]*@leetcode\\.com$';
+
+# ^ : means start from initial
+# [] : means charset
+# a-z : - denotes range
+# + : we want atleast one character at initial place
+# * : means 0 or more character
+# \\ : denotes we want . as it is, don't change it
+# $ : means at the end of string
